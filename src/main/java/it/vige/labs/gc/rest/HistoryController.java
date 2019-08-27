66efb7c7d1053f99437c678799ee769c7c1055b8
flashboss <luca.stancapiano@vige.it>
2019-08-27 14:23:40 +0200

    history with mongodb

--- src/main/java/it/vige/labs/gc/rest/HistoryController.java
+++ src/main/java/it/vige/labs/gc/rest/HistoryController.java
@@ -1,7 +1,14 @@
 package it.vige.labs.gc.rest;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
 
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,12 +16,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 import it.vige.labs.gc.result.Voting;
 import it.vige.labs.gc.votingpapers.VotingPapers;
 
 @RestController
 @CrossOrigin(origins = "*")
 public class HistoryController {
+
+	private Logger logger = LoggerFactory.getLogger(HistoryController.class);
+
+	private String DB_HOST = "localhost";
+	private int DB_PORT = 27017;
 
 	@GetMapping(value = "/prepare")
 	public boolean prepare(@RequestParam boolean start) {
@@ -35,16 +51,43 @@ public class HistoryController {
 
 	@PostMapping(value = "/configure")
 	public Affluences configure(Affluences affluences) {
+		execute(database -> {
+			MongoCollection<Document> collection = database.getCollection("affluences");
+			for (Date date : affluences.getAffluences()) {
+				Document document = new Document();
+				document.put("date", date);
+				collection.insertOne(document);
+			}
+			return "";
+		});
 		return affluences;
 	}
 
 	@GetMapping(value = "/getVotingPapers")
 	public VotingPapers getVotingPapers(@RequestParam Date date) {
+		@SuppressWarnings("unchecked")
+		List<Date> dates = (List<Date>) execute(database -> {
+			MongoCollection<Document> collection = database.getCollection("affluences");
+			List<Date> list = new ArrayList<Date>();
+			Iterator<Document> found = collection.find().iterator();
+			while (found.hasNext()) {
+				list.add((Date) found.next().get("date"));
+			}
+			return list;
+		});
+		logger.info(dates + "");
 		return new VotingPapers();
 	}
 
 	@GetMapping(value = "/getVoting")
 	public Voting getVoting(@RequestParam Date date) {
 		return null;
+	}
+
+	private Object execute(Function<MongoDatabase, Object> function) {
+		try (MongoClient mongoClient = new MongoClient(DB_HOST, DB_PORT)) {
+			MongoDatabase database = mongoClient.getDatabase("history");
+			return function.apply(database);
+		}
 	}
 }
